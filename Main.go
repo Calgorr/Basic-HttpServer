9e3af837af1a0ad8959a991b4dfc0cd9584d7633
Calgor <ahomayobi684@gmail.com>
2022-09-01 13:15:37 +0430

    to be continued

--- Main.go
+++ Main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -9,13 +10,13 @@ func hello(w http.ResponseWriter, req *http.Request) {
 	w.Write([]byte("Hello \nplease Enter (https://localhost:8090/goByExamples)!"))
 }
 
-func headers(w http.ResponseWriter, req *http.Request) {
-
-	for name, headers := range req.Header {
-		for _, h := range headers {
-			fmt.Fprintf(w, "%v: %v\n", name, h)
-		}
+func headers(w http.ResponseWriter, r *http.Request) {
+	bytes, err := ioutil.ReadFile("index.html")
+	if err != nil {
+		fmt.Println(err)
 	}
+	fmt.Fprintf(w, string(bytes))
+	fmt.Print("salam")
 }
 
 func main() {
